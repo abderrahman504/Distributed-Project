03a0deb96497a57d988ac78d5c998a360fa8febb
Patrick-Geo7 <patrick.georges18@gmail.com>
2025-05-17 15:20:43 +0300

    BFS

--- server/src/GraphOperator.java
+++ server/src/GraphOperator.java
@@ -32,9 +32,59 @@ public class GraphOperator {
 		if(graph.containsKey(from)) graph.get(from).remove(to);
 	}
 
-	protected static void findPath(HashMap<Integer, HashSet<Integer>> graph, int from, int to)
-	{
-		// implement later
+
+	protected static void findPath(HashMap<Integer, HashSet<Integer>> graph, int from, int to) {
+		if (from == to) {
+			System.out.println("Shortest path: " + from);
+			return;
+		}
+
+		Queue<Integer> queue = new LinkedList<>();
+		Map<Integer, Integer> parent = new HashMap<>();
+		Set<Integer> visited = new HashSet<>();
+
+		queue.offer(from);
+		visited.add(from);
+		parent.put(from, null);
+
+		boolean found = false;
+
+		while (!queue.isEmpty()) {
+			int current = queue.poll();
+
+			for (int neighbor : graph[current]) {
+				if (!visited.contains(neighbor)) {
+					visited.add(neighbor);
+					parent.put(neighbor, current);
+					queue.offer(neighbor);
+
+					if (neighbor == to) {
+						found = true;
+						break;
+					}
+				}
+			}
+
+			if (found) break;
+		}
+
+		if (!found) {
+			System.out.println("No path exists between " + from + " and " + to);
+			return;
+		}
+
+		// Reconstruct the path from 'to' to 'from'
+		List<Integer> path = new ArrayList<>();
+		Integer current = to;
+		while (current != null) {
+			path.add(current);
+			current = parent.get(current);
+		}
+
+		// Reverse the path to get 'from' to 'to'
+		Collections.reverse(path);
+		System.out.println("Shortest path: " + path);
 	}
+
 	
 }
