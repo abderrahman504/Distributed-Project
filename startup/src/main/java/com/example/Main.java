c75f1336e0b9f158ce4eb6654e029e60f80e4b30
abderrahman504 <rahmankhsherb@gmail.com>
2025-05-20 19:22:32 +0300

    Complete. Now we don't specify the hostname of the server or ports for the rmi registry

--- startup/src/main/java/com/example/Main.java
+++ startup/src/main/java/com/example/Main.java
@@ -10,23 +10,40 @@ public class Main {
 
     private static String serverCommand;
     private static List<String> clientCommands;
+	private static int graphSize, batchSize, sleep;
+	private static float writeRatio;
+
 
     public static void main(String[] args) {
         try {
             loadProperties("system.properties");
 
+			List<Process> processes = new ArrayList<>();
             // Start the server
-            System.out.println("Starting server at " + serverIP);
-            runRemoteCommand(serverIP, serverCommand);
+            System.out.println("Starting server");
+            processes.add(runCommand(serverCommand));
 			Thread.sleep(5000);
             // Start all clients
             for (int i=0; i<clientCommands.size(); i++) {
-                System.out.println("Starting client");
-                runRemoteCommand("", clientCommands.get(i));
+                System.out.println("Starting client " + i);
+                processes.add(runCommand(clientCommands.get(i)));
             }
 
+			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+				@Override
+				public void run() {
+					for (Process p : processes){
+						p.destroy();
+						try{
+							p.waitFor();
+						} catch (Exception e){
+							p.destroyForcibly();
+						}
+					}	
+				}
+			}));
 
-
+			while(true) {}
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -40,19 +57,21 @@ public class Main {
 		
 		
 		int clientCount = Integer.parseInt(props.getProperty("GSP.numberOfNodes"));
+		graphSize = Integer.parseInt(props.getProperty("client.graphSize"));
+		batchSize = Integer.parseInt(props.getProperty("client.batchSize"));
+		writeRatio = Float.parseFloat(props.getProperty("client.writeRatio"));
+		sleep = Integer.parseInt(props.getProperty("client.sleepInterval"));
 		
         clientCommands = new ArrayList<>(clientCount);
-		clientIPs = new ArrayList<>(clientCount);
 		serverCommand = "java -jar Server.jar";
 		for(int i=0; i<clientCount; i++){
-			// clientIPs.add(props.getProperty("GSP.node"+i));
-			String cmd = String.format("java -jar Client.jar %d false -random 20 50 0.7 5000", i);
+			String cmd = String.format("java -jar Client.jar %d false -random %d %d %f %d", i, graphSize, batchSize, writeRatio, sleep);
 			clientCommands.add(cmd);
 		}
     }
 
-    private static void runRemoteCommand(String host, String command) throws Exception {
-        new ProcessBuilder(command.split(" ")).inheritIO().start();
+    private static Process runCommand(String command) throws Exception {
+        return new ProcessBuilder(command.split(" ")).inheritIO().start();
     }
 }
 
